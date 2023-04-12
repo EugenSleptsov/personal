2fdebec13888426429447bcedeced71e8d31ea78
Evgeny S <dalikimail@gmail.com>
2023-04-12 13:07:40 +0300

    optimized base36

--- go/base_converter.go
+++ go/base_converter.go
@@ -5,44 +5,7 @@ import (
     "strings"
 )
 
-var base36 = map[int]string{
-    0: "0",
-    1: "1",
-    2: "2",
-    3: "3",
-    4: "4",
-    5: "5",
-    6: "6",
-    7: "7",
-    8: "8",
-    9: "9",
-    10: "A",
-    11: "B",
-    12: "C",
-    13: "D",
-    14: "E",
-    15: "F",
-    16: "G",
-    17: "H",
-    18: "I",
-    19: "J",
-    20: "K",
-    21: "L",
-    22: "M",
-    23: "N",
-    24: "O",
-    25: "P",
-    26: "Q",
-    27: "R",
-    28: "S",
-    29: "T",
-    30: "U",
-    31: "V",
-    32: "W",
-    33: "X",
-    34: "Y",
-    35: "Z",
-}
+var base36 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 func Converter(num float64, precision int, base float64) string {
     if num == 0 {
@@ -62,7 +25,7 @@ func Converter(num float64, precision int, base float64) string {
         }
         digit := int(math.Mod(math.Floor((num / math.Pow(base, float64(i)))), base))
         num -= float64(digit) * math.Pow(base, float64(i))
-        result += base36[digit]
+        result += string(base36[digit])
     }
 
     if isNegative {
